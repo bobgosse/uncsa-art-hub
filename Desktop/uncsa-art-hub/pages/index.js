f583a67d109b9f7612716d64ed2c2a2b2d8ca671
Bob Gosse <bobgosse@gmail.com>
2025-06-29 18:12:41 -0400

    Add homepage to fix Vercel 404

---
+++ Desktop/uncsa-art-hub/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to UNCSA Art Hub</h1>
+      <p>This is your new project management site for student films.</p>
+      <p>Visit the <a href="/test">Test Page</a> to see Supabase in action.</p>
+    </div>
+  );
+}
